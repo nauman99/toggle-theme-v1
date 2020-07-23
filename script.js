455e07d5c827ccfda153f18bc14ce5d44392b125
Nauman Ahmad <nauman2077@gmail.com>
2020-07-24 02:07:36 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let darkMode = false,
+  lightMode = true;
+
+function switchTheme() {
+  if (darkMode === false) {
+    darkMode = true;
+    lightMode = false;
+    document.querySelector("body").style.backgroundColor = "black";
+    document.querySelector("body").style.color = "white";
+    document.querySelector("span").style.color = "yellow";
+    document.getElementById("icon").src = "dark.PNG";
+  } else if (darkMode === true) {
+    darkMode = false;
+    lightMode = true;
+    document.querySelector("body").style.backgroundColor = "white";
+    document.querySelector("body").style.color = "black";
+    document.querySelector("span").style.color = "royalblue";
+
+    document.getElementById("icon").src = "light.PNG";
+  }
+}
+function multiply(a, b) {
+  return a * b;
+}
+
+multiply(2, 3);

8d82d17f33f0caeedbcfc632e2d4faad41affd30
Nauman Ahmad <nauman2077@gmail.com>
2020-08-11 00:45:15 +0500

    made changes

--- script.js
+++ script.js
@@ -2,31 +2,43 @@ let darkMode = false,
   lightMode = true;
 
 const bodyStyle = document.querySelector("body").style,
-  spanStyle = document.querySelector("span").style,
+  accentStyle = document.getElementsByClassName("accent"),
   icon = document.getElementById("icon"),
-  headerStyle = document.querySelector("header").style;
+  headerStyle = document.querySelector("header").style,
+  codeStyle = document.getElementsByClassName("code-snippet");
 
 function switchTheme() {
-  
   if (darkMode === false) {
     darkMode = true;
     lightMode = false;
 
+    icon.src = "dark.PNG";
     bodyStyle.backgroundColor = "black";
     bodyStyle.color = "white";
-    spanStyle.color = "yellow";
-
-    icon.src = "dark.PNG";
     headerStyle.backgroundColor = "black";
+
+    for (var i = 0; i < accentStyle.length; i++) {
+      accentStyle[i].style.color = "yellow";
+    }
+
+    for (var i = 0; i < codeStyle.length; i++) {
+      codeStyle[i].style.backgroundColor = "#1c1919";
+    }
   } else if (darkMode === true) {
     darkMode = false;
     lightMode = true;
 
+    icon.src = "light.PNG";
     bodyStyle.backgroundColor = "white";
     bodyStyle.color = "black";
-    spanStyle.color = "royalblue";
-
-    icon.src = "light.PNG";
     headerStyle.backgroundColor = "white";
+
+    for (var i = 0; i < accentStyle.length; i++) {
+      accentStyle[i].style.color = "royalblue";
+    }
+
+    for (var i = 0; i < codeStyle.length; i++) {
+      codeStyle[i].style.backgroundColor = "#f4f4f4";
+    }
   }
 }
